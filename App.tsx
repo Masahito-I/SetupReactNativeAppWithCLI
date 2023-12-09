f02824e1bd8379e9f396722a002a8f4f262789e4
Imasa <imiplayacc@gmail.com>
2023-12-10 04:51:35 +0900

    Removed ios parts and unnecessary comments.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,6 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
  * @format
  */
-
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
