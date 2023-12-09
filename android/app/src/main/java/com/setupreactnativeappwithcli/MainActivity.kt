346e2ea517625b8b3c7854337bf50ce3959925ce
Imasa <imiplayacc@gmail.com>
2023-12-10 07:06:31 +0900

    Renamed the app

--- android/app/src/main/java/com/setupreactnativeappwithcli/MainActivity.kt
+++ android/app/src/main/java/com/setupreactnativeappwithcli/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "SetupReactNativeAppWithCLI"
+  override fun getMainComponentName(): String = "setup-react-native-app-with-cli"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
